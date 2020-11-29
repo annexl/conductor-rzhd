b135ee3cc58f964c8945ed55bfd79a777d6aa5ec
annexl <75204995+annexl@users.noreply.github.com>
2020-11-29 15:56:38 +0400

    Update bd.php

--- bd.php
+++ bd.php
@@ -1,9 +1,4 @@
 <?php
-//Подключаемся к mysql серверу
-//имя - localhost
-//юзер - root
-//пароль - нету
 $mysql_connect=mysql_connect("localhost","host1400948_rzd","43439201+-");
-//Выбираем базу данных mysite
 $db=mysql_select_db("host1400948_rzd");
 ?>
